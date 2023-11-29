084e7ae5805d6f7bb690d467985dff8130471bc7
Grindelwald <grindelwald@mail.com>
2023-11-29 21:33:53 +0100

    Update script.js
    Signed-off-by: Grindelwald <grindelwald@mail.com>

--- docs/script.js
+++ docs/script.js
@@ -1,16 +1,16 @@
 function switchTheme() {
 	if (document.body.getAttribute('theme') === 'light') {
 		document.body.setAttribute('theme', 'dark');
-		document.getElementById('theme-icon').setAttribute('class', 'fa-solid fa-moon');
+		document.getElementById('theme-icon').setAttribute('src', 'icons/theme-dark.svg');
 		return true;
 	}
 	if (document.body.getAttribute('theme') === 'dark') {
 		document.body.setAttribute('theme', 'light');
-		document.getElementById('theme-icon').setAttribute('class', 'fa-solid fa-sun');
+		document.getElementById('theme-icon').setAttribute('src', 'icons/theme-light.svg');
 		return true;
 	}
 	console.log('theme not found');
 	document.body.setAttribute('theme', 'light');
-	document.getElementById('theme-icon').setAttribute('class', 'fa-solid fa-sun');
+	document.getElementById('theme-icon').setAttribute('src', 'icons/theme-light.svg');
 	return false;
 }
