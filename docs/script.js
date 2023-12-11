b8748639dc3c2c284fd0e7922594ffb9bb37fe97
Grindelwald <grindelwald@mail.com>
2023-12-11 19:56:04 +0100

    Update script.js
    Signed-off-by: Grindelwald <grindelwald@mail.com>

--- docs/script.js
+++ docs/script.js
@@ -1,22 +1,28 @@
 const urlParams = new URLSearchParams(window.location.search);
-if (urlParams.has('theme')) {
-	let theme = urlParams.get('theme');
-	document.body.setAttribute('theme', theme);
-}
+if (urlParams.has('theme'))
+	theme(urlParams.get('theme'));
 function switchTheme() {
 	if (document.body.getAttribute('theme') === 'light') {
-		document.body.setAttribute('theme', 'dark');
-		document.getElementById('theme-icon').setAttribute('src', 'icons/theme-dark.svg');
-		document.getElementById('theme-icon').setAttribute('alt', 'switch theme from dark to light');
+		theme('dark');
 		return;
 	}
 	if (document.body.getAttribute('theme') === 'dark') {
+		theme('light');
+		return;
+	}
+	theme('light');
+}
+function theme(value) {
+	if (value === 'light') {
 		document.body.setAttribute('theme', 'light');
 		document.getElementById('theme-icon').setAttribute('src', 'icons/theme-light.svg');
 		document.getElementById('theme-icon').setAttribute('alt', 'switch theme from light to dark');
 		return;
 	}
-	document.body.setAttribute('theme', 'light');
-	document.getElementById('theme-icon').setAttribute('src', 'icons/theme-light.svg');
-	document.getElementById('theme-icon').setAttribute('alt', 'switch theme from light to dark');
+	if (value === 'dark') {
+		document.body.setAttribute('theme', 'dark');
+		document.getElementById('theme-icon').setAttribute('src', 'icons/theme-dark.svg');
+		document.getElementById('theme-icon').setAttribute('alt', 'switch theme from dark to light');
+		return;
+	}
 }
